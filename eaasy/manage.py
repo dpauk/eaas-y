e923daecd6e2339a71abe44890e8ad53c8af9a30
David Albone <davd.p.albone@gmail.com>
2018-04-29 14:02:46 +0100

    Fixed Travis with Docker:

--- eaasy/manage.py
+++ eaasy/manage.py
@@ -1,9 +1,14 @@
 from flask.cli import FlaskGroup
+import pytest 
 
 from eaasy import app
 
 cli = FlaskGroup(app)
 
+# @cli.command
+# def test():
+#     """Runs tests without code coverage"""
+#     pytest.
 
 if __name__ == '__main__':
     cli()
