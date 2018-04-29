351f1331a6cf3669db8597a9a7884fb2f9ba9b64
David Albone <davd.p.albone@gmail.com>
2018-04-29 14:20:38 +0100

    Trying to fix broken Travis build

--- eaasy/manage.py
+++ eaasy/manage.py
@@ -1,14 +1,16 @@
 from flask.cli import FlaskGroup
-# import pytest 
+import pytest 
 
 from project import app
 
 cli = FlaskGroup(app)
 
-# @cli.command
-# def test():
-#     """Runs tests without code coverage"""
-#     pytest.
+
+@cli.command
+def test():
+    """Runs tests without code coverage"""
+    pytest.main()
+
 
 if __name__ == '__main__':
     cli()
