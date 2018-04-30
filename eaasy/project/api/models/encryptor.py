9257a596a8ec03c5196dad791d336a8e28574ab2
David Albone <davd.p.albone@gmail.com>
2018-04-30 20:35:43 +0100

    Added the cryptography library so that encryption actually works

--- eaasy/project/api/models/encryptor.py
+++ eaasy/project/api/models/encryptor.py
@@ -1,5 +1,12 @@
+import os
+
+from cryptography.fernet import Fernet
+
+
 class Encryptor:
     def encrypt(self, plaintext):
         if plaintext == "" or not isinstance(plaintext, str):
             raise ValueError
-        return "sksjdfklnnlfsk"
+        key = bytes(os.environ["ENCRYPTION_KEY"], "utf-8").decode("unicode_escape")
+        f = Fernet(key)
+        return f.encrypt(bytes(plaintext, "utf-8")).decode("utf-8")
